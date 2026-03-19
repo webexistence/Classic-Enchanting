eabe05df9e6c6b9839ecc881507951586dea06bd
domi <scavcrab@gmail.com>
2026-03-18 19:28:45 -0500

    Initial commit

---
+++ src/client/java/io/github/webexistence/classicenchanting/ClassicEnchantingClient.java
@@ -0,0 +1,10 @@
+package io.github.webexistence.classicenchanting;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClassicEnchantingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/io/github/webexistence/classicenchanting/mixin/client/EnchantmentScreenMixin.java
@@ -0,0 +1,21 @@
+package io.github.webexistence.classicenchanting.mixin.client;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.ingame.EnchantmentScreen;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+@Mixin(EnchantmentScreen.class)
+public class EnchantmentScreenMixin {
+    @Inject(method = "render", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/client/gui/DrawContext;drawTooltip(Lnet/minecraft/client/font/TextRenderer;Ljava/util/List;II)V"))
+    private void removeEnchantClue(DrawContext context, int mouseX, int mouseY, float deltaTicks, CallbackInfo ci, @Local(name = "list") List<Text> enchantTooltipTextList) {
+        enchantTooltipTextList.subList(0, 2).clear();
+    }
+}
