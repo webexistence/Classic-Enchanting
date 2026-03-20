dd1696a4e7e640ae841e7d544c2625744056c804
domi <scavcrab@gmail.com>
2026-03-20 17:11:00 -0500

    Update EnchantmentHelperMixin.java to instead use @ModifyVariable; make server calculate using actual enchantmentPower (level requirement) and a hard-coded multiplier

--- src/main/java/io/github/webexistence/classicenchanting/mixin/EnchantmentHelperMixin.java
+++ src/main/java/io/github/webexistence/classicenchanting/mixin/EnchantmentHelperMixin.java
@@ -1,21 +1,33 @@
 package io.github.webexistence.classicenchanting.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.screen.EnchantmentScreenHandler;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(EnchantmentScreenHandler.class)
 public class EnchantmentHelperMixin {
-    @ModifyArg(
-            method = "method_17410",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/entity/player/PlayerEntity;applyEnchantmentCosts(Lnet/minecraft/item/ItemStack;I)V"
-            ),
-            index = 1
+    @Shadow
+    @Final
+    public int[] enchantmentPower;
+
+    /**
+     * Modifies the original enchantment cost (server-side) based on level required and a multiplier.
+     * @param   originalEnchantmentCost "i"; Equal to (enchantmentIndex + 1). Values (1,2,3). Unused.
+     * @param   enchantmentIndex        "id"; Index of enchantment. Values (0,1,2).
+     * return                           The modified enchantment cost.
+     */
+    @ModifyVariable(
+            method = "onButtonClick",
+            at = @At(value = "STORE"),
+            name = "i"
     )
-    private int changeEnchantmentLevelCost(int originalCost) {
-        return originalCost * 3;
+    private int changeEnchantmentCost(int originalEnchantmentCost, @Local(name = "id") int enchantmentIndex) {
+        float enchantmentLevelCostMultiplier = 0.5F; // TODO: make this configurable via json
+        // enchantmentPower[0,1,2] contains the level requirements (max is 30).
+        return Math.round(this.enchantmentPower[enchantmentIndex] * enchantmentLevelCostMultiplier);
     }
 }
