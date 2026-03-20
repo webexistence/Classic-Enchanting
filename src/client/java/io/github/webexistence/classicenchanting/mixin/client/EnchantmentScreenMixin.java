380138395f06f3d58c31c01e24d8f6bb0fd534f5
domi <scavcrab@gmail.com>
2026-03-20 17:58:54 -0500

    Implement client-side tooltip display of new modified enchantment cost; borrow logic from existing server-side logic (EnchantmentHelperMixin.java)

--- src/client/java/io/github/webexistence/classicenchanting/mixin/client/EnchantmentScreenMixin.java
+++ src/client/java/io/github/webexistence/classicenchanting/mixin/client/EnchantmentScreenMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
@@ -17,5 +18,24 @@ public class EnchantmentScreenMixin {
             target = "Lnet/minecraft/client/gui/DrawContext;drawTooltip(Lnet/minecraft/client/font/TextRenderer;Ljava/util/List;II)V"))
     private void removeEnchantClue(DrawContext context, int mouseX, int mouseY, float deltaTicks, CallbackInfo ci, @Local(name = "list") List<Text> enchantTooltipTextList) {
         enchantTooltipTextList.subList(0, Math.min(2, enchantTooltipTextList.size())).clear();
+    }
+
+    /**
+     * Modifies the displayed enchantment cost (client-side) based on level required and a multiplier.
+     * @param   originalEnchantmentCost "m"; Equal to (enchantmentIndex + 1). Values (1,2,3). Unused.
+     * @param   enchantmentPowerInt     "k"; Experience level required to enchant.
+     * return                           The modified enchantment cost.
+     */
+    @ModifyVariable(
+            method = "render",
+            at = @At(value = "STORE"),
+            name = "m"
+    )
+    private int changeEnchantmentCostClient(
+            int originalEnchantmentCost,
+            @Local(name = "k") int enchantmentPowerInt
+    ) {
+        float enchantmentLevelCostMultiplier = 0.5F; // TODO: make this configurable via json
+        return Math.round(enchantmentPowerInt * enchantmentLevelCostMultiplier);
     }
 }
