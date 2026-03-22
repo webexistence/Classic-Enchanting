e78339f4ed41a3e0312e10c3a2f1f5cd839942e2
domi <scavcrab@gmail.com>
2026-03-22 15:56:04 -0500

    Fix calculateEnchantmentCost() doc comment grammar

--- src/main/java/io/github/webexistence/classicenchanting/util/EnchantmentCostHelper.java
+++ src/main/java/io/github/webexistence/classicenchanting/util/EnchantmentCostHelper.java
@@ -8,7 +8,7 @@ public class EnchantmentCostHelper {
     /**
      * Calculate new enchantment cost based on power and a multiplier.
      * Rounds down to be more generous to lower-level enchantments.
-     * @param 	enchantmentPowerInt	The original level requirements (max 30).
+     * @param 	enchantmentPowerInt	The original level requirement (max 30).
      * @return						Integer enchantment cost.
      */
     public static int calculateEnchantmentCost(int enchantmentPowerInt) {
