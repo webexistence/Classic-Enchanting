e6c3342614d24ea46ac746f9364c220d60e0ac3f
domi <scavcrab@gmail.com>
2026-03-27 17:37:25 -0500

    Fix lapis cost; add new method calculateCostByIndex() to hold shared cost calculation logic

--- src/main/java/io/github/webexistence/classicenchanting/mixin/EnchantmentScreenHandlerMixin.java
+++ src/main/java/io/github/webexistence/classicenchanting/mixin/EnchantmentScreenHandlerMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.screen.EnchantmentScreenHandler;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.*;
 
 @Mixin(EnchantmentScreenHandler.class)
@@ -12,6 +13,19 @@ public class EnchantmentScreenHandlerMixin {
     @Shadow
     @Final
     public int[] enchantmentPower;
+
+    /**
+     * The mixins in this class for modifying cost both rely on source "i = id + 1"; This logic deduces "id", the
+     * enchantmentIndex, from "i", the originalCost. We subtract 1 to cancel out the "+ 1" and set our own cost based
+     * on the enchantmentPower.
+     * @param originalCost Vanilla cost.
+     * @return Modified cost.
+     */
+    @Unique
+    private int calculateCostByIndex(int originalCost) {
+        int enchantmentIndex = originalCost - 1;
+        return EnchantmentCostHelper.calculateEnchantmentCost(this.enchantmentPower[enchantmentIndex]);
+    }
 
     /**
      * Modifies enchantment level cost (server-side) based on level required and a multiplier.
@@ -27,7 +41,23 @@ public class EnchantmentScreenHandlerMixin {
             index = 1
     )
     private int changeEnchantmentCostServer(int originalLevelCost) {
-        int enchantmentIndex = originalLevelCost - 1;
-        return EnchantmentCostHelper.calculateEnchantmentCost(this.enchantmentPower[enchantmentIndex]);
+        return calculateCostByIndex(originalLevelCost);
+    }
+
+    /**
+     * Modifies lapis cost (server-side) based on level required and a multiplier.
+     * @param originalLapisCost "i"; Vanilla lapis cost. Used to deduce enchantmentIndex.
+     * @return Modified lapis cost.
+     */
+    @ModifyArg(
+            method = "method_17410", // lambda method inside onButtonClick()
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/item/ItemStack;decrementUnlessCreative(ILnet/minecraft/entity/LivingEntity;)V"
+            ),
+            index = 0
+    )
+    private int changeLapisCostServer(int originalLapisCost) {
+        return calculateCostByIndex(originalLapisCost);
     }
 }
