49850df6e7ef6ebae47226c501e0aa45efa92688
domi <scavcrab@gmail.com>
2026-04-05 09:52:19 -0500

    Change default cost multiplier to 0.5 instead of 1.0

--- src/main/java/io/github/webexistence/classicenchanting/config/ClassicEnchantingConfig.java
+++ src/main/java/io/github/webexistence/classicenchanting/config/ClassicEnchantingConfig.java
@@ -9,7 +9,7 @@ public class ClassicEnchantingConfig extends MidnightConfig {
     public static final String GENERAL = "general";
 
     @Entry(category = GENERAL, isSlider = true, min=0.5, max=1.0, precision = 4)
-    public static double enchantmentLevelCostMultiplier = 1.0;
+    public static double enchantmentLevelCostMultiplier = 0.5;
     @Entry(category = GENERAL)
     public static boolean enableGrindstoneMultiplier = true;
 }
